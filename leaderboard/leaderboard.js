b1b19694cd81f5b0c18292de3a429bb9a50a5898
Abhay Chauhan <chauhan27.abhay@gmail.com>
2021-03-16 18:43:39 +0530

    leaderboard completed

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -1,9 +1,12 @@
-
 const request = require("request");
 const cheerio = require("cheerio");
-const singleMatchExtractor = require("./singleMatch");
 
 url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-results"
+
+let statsArr = [];
+let leaderBoardArr = [];
+let gCount;
+let count = 1;
 
 request(url, cb);
 
@@ -15,15 +18,92 @@ function cb(error, response, html){
         let $ = cheerio.load(html);
 
         let matchCards = $(".col-md-8.col-16");
-
+        gCount = matchCards.length;
         for(let i=0; i<matchCards.length; i++){
             let container = $(matchCards[i]).find(".match-cta-container .btn.btn-sm.btn-outline-dark.match-cta");
 
             let link = $(container[2]).attr("href");
             let fullLink = "https://www.espncricinfo.com"+link;
 
-            singleMatchExtractor.fn(fullLink);
+           request(fullLink, cbsingle);
         }
     }
-    console.log("completed!!!")
+}
+
+function cbsingle(error, response, html){
+    if(error){
+        console.log("some error occured");
+        return;
+    }
+    let $ = cheerio.load(html);
+
+    let bothTeams = $(".event .teams>.team");
+    let myTeam;
+    for(let i=0; i<bothTeams.length; i++){
+        let lossingTeam = $(bothTeams[i]).hasClass("team-gray");
+        if(lossingTeam === false){
+            myTeam = $(bothTeams[i]).find(".name-detail .name-link").text();
+        }
+    }
+    
+    let colBlock = $(".Collapsible");
+
+    let bothInningsTeamName = $(".Collapsible .header-title.label");
+    for(let i=0; i<bothInningsTeamName.length; i++){
+        let teamName = $(bothInningsTeamName[i]).text().split("INNINGS")[0].trim();
+
+        if(teamName === myTeam){
+            console.log(myTeam);
+            let winTeamColBlock = $(colBlock[i]);
+            printTeamStats(winTeamColBlock, $);
+        }
+    }
+
+    if (gCount == count) {
+            leaderBoardArr.sort((a, b) => b.runs-a.runs);
+            console.table(leaderBoardArr);
+            console.log("Completed!!")
+    } else {
+        count++;
+    }
+}
+
+function printTeamStats(winTeamColBlock, $){
+    let BatsmanRows = $(winTeamColBlock).find(".table.batsman tbody tr");
+
+    for(let i=0; i<BatsmanRows.length; i++){
+        let batsmanStats = $(BatsmanRows[i]).find("td");
+
+        if(batsmanStats.length === 8){
+            let playerName = $(batsmanStats[0]).text();
+            let runs = $(batsmanStats[2]).text();
+
+            statsArr.push({
+                Name: playerName,
+                Runs: runs
+            })
+
+            addToLeaderBoard(playerName, runs);
+        }
+    }
+    console.table(statsArr);
+    console.log("``````````````````````````````````````````````");
+    statsArr = [];
+}
+
+function addToLeaderBoard(playerName, cruns){
+    doesExist = false;
+    for(let i=0; i<leaderBoardArr.length; i++){
+        if(leaderBoardArr[i].name == playerName){
+            leaderBoardArr[i].runs += Number(cruns);
+            doesExist = true;
+            break;
+        }
+    }
+    if(doesExist == false){
+        leaderBoardArr.push({
+            name: playerName,
+            runs: Number(cruns)
+        })
+    }
 }
